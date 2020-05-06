09afc49566e6e8ea54153b25c4935b9f6e7dc110
Ben Hayes <benh@andrew.cmu.edu>
2020-05-05 21:08:08 -0400

    Add initial log, python script, and batch file

---
+++ ping_script.bat
@@ -0,0 +1,2 @@
+call [path to Anaconda directory]\Scripts\activate.bat [path to Anaconda directory]
+[path to Anaconda directory]\envs\[conda environment name]\python [path to python script]\ping_script.py
---
+++ ping_script.py
@@ -0,0 +1,31 @@
+import pythonping as pp
+import datetime as dt
+import pandas as pd
+import time
+
+# Define parameters
+url = '8.8.8.8'  # IP Address or Hostname
+log_path = ''  # 'C:/Users/{yourusername}/Downloads/ping_log.csv'
+MAX_TIMEOUT_SECS = 1.00  # Amount of time to wait before response time out (1.0 seconds = 1000ms)
+PING_COUNT = 4  # If you change this, add/remove the appropriate amount of columns to the CSV.
+
+# Ping
+try:
+    # Ping url 3 times
+    # print("Pinging {}".format(url))
+    responses = pp.ping(url, MAX_TIMEOUT_SECS, PING_COUNT)._responses
+    
+    response_dict = {'ping{}'.format(i+1): response.time_elapsed_ms for i, response in enumerate(responses)}
+    response_dict['datetime'] = dt.datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    # # DEBUG: Print out responses
+    # for response in responses:
+    #     print(response.time_elapsed_ms)
+
+    # File i/o and update
+    df_ping_log = pd.read_csv(log_path)
+    df_ping_log = df_ping_log.append(response_dict, ignore_index=True)
+    df_ping_log.to_csv(log_path, index=False)
+
+except:
+   print("Error pinging {} - no log entry added".format(url))
